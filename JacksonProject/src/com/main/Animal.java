bba29a2d3ead81c0ed50063d435bcf7729356f34
sherinmathew <sherinmathew@192.168.1.106>
2017-07-10 20:33:26 +0530

    initial message

---
+++ JacksonProject/src/com/main/Animal.java
@@ -0,0 +1,18 @@
+package com.main;
+
+public class Animal {
+private String typeofEye;
+private boolean isDomestic;
+public String getTypeofEye() {
+	return typeofEye;
+}
+public void setTypeofEye(String typeofEye) {
+	this.typeofEye = typeofEye;
+}
+public boolean isDomestic() {
+	return isDomestic;
+}
+public void setDomestic(boolean isDomestic) {
+	this.isDomestic = isDomestic;
+}
+}
---
+++ JacksonProject/src/com/main/JacksonMain.java
@@ -0,0 +1,27 @@
+package com.main;
+
+import java.io.IOException;
+
+import org.codehaus.jackson.map.ObjectMapper;
+
+public class JacksonMain {
+
+	public static void main(String[] args) {
+		ObjectMapper objectMapper = new ObjectMapper();
+		String jsonString = "{\"name\":\"Mahesh\",\"age\":21}";
+		Student student;
+		Animal animal= new Animal();
+		animal.setDomestic(true);
+		animal.setTypeofEye("RED");
+		try {
+			student = objectMapper.readValue(jsonString, Student.class);
+			System.out.println("sherin the student is "+student.getName()); 
+			System.out.println(objectMapper.writeValueAsString(student));
+			System.out.println(objectMapper.writeValueAsString(animal));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+}
